2ae7eea2c7fcd08dbefdcb39005d62334ce4963b
0xBAMA <jon.baker1222@gmail.com>
2021-04-03 17:12:17 -0400

    Revert "convert to latest nqae version - still need to bring over shaders, etc"
    This reverts commit 50880a1018976629c6ceb5bcff154b57fd95e730.

--- resources/engine_code/engine_utils.cc
+++ resources/engine_code/engine_utils.cc
@@ -123,13 +123,11 @@ void engine::create_window() {
   SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
   SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 
-  // depth testing not relevant
-  // SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-  // SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
-  // multisampling not relevant
-  // SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-  // SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 8);
+  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 8);
 
   // this is how you query the screen resolution
   SDL_DisplayMode dm;
@@ -173,8 +171,7 @@ void engine::create_window() {
   if (gl3wInit() != 0)
     fprintf(stderr, "Failed to initialize OpenGL loader!\n");
 
-  // depth testing not relevant
-  // glEnable(GL_DEPTH_TEST);
+  glEnable(GL_DEPTH_TEST);
   // glEnable(GL_LINE_SMOOTH);
 
   glPointSize(3.0);
@@ -213,66 +210,55 @@ void engine::create_window() {
   cout << "done." << endl;
 
   ImVec4 *colors = ImGui::GetStyle().Colors;
-  colors[ImGuiCol_Text] = ImVec4(0.67f, 0.50f, 0.16f, 1.00f);
-  colors[ImGuiCol_TextDisabled] = ImVec4(0.33f, 0.27f, 0.16f, 1.00f);
-  colors[ImGuiCol_WindowBg] = ImVec4(0.10f, 0.05f, 0.00f, 1.00f);
-  colors[ImGuiCol_ChildBg] = ImVec4(0.23f, 0.17f, 0.02f, 0.05f);
-  colors[ImGuiCol_PopupBg] = ImVec4(0.30f, 0.12f, 0.06f, 0.94f);
-  colors[ImGuiCol_Border] = ImVec4(0.25f, 0.18f, 0.09f, 0.33f);
-  colors[ImGuiCol_BorderShadow] = ImVec4(0.33f, 0.15f, 0.02f, 0.17f);
-  colors[ImGuiCol_FrameBg] = ImVec4(0.561f, 0.082f, 0.04f, 0.17f);
-  colors[ImGuiCol_FrameBgHovered] = ImVec4(0.19f, 0.09f, 0.02f, 0.17f);
-  colors[ImGuiCol_FrameBgActive] = ImVec4(0.25f, 0.12f, 0.01f, 0.78f);
-  colors[ImGuiCol_TitleBg] = ImVec4(0.25f, 0.12f, 0.01f, 1.00f);
-  colors[ImGuiCol_TitleBgActive] = ImVec4(0.33f, 0.15f, 0.02f, 1.00f);
-  colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.25f, 0.12f, 0.01f, 1.00f);
-  colors[ImGuiCol_MenuBarBg] = ImVec4(0.14f, 0.07f, 0.02f, 1.00f);
-  colors[ImGuiCol_ScrollbarBg] = ImVec4(0.13f, 0.10f, 0.08f, 0.53f);
-  colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.25f, 0.12f, 0.01f, 0.78f);
-  colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.33f, 0.15f, 0.02f, 1.00f);
-  colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.25f, 0.12f, 0.01f, 0.78f);
-  colors[ImGuiCol_CheckMark] = ImVec4(0.69f, 0.45f, 0.11f, 1.00f);
-  colors[ImGuiCol_SliderGrab] = ImVec4(0.28f, 0.18f, 0.06f, 1.00f);
-  colors[ImGuiCol_SliderGrabActive] = ImVec4(0.36f, 0.22f, 0.06f, 1.00f);
-  colors[ImGuiCol_Button] = ImVec4(0.25f, 0.12f, 0.01f, 0.78f);
-  colors[ImGuiCol_ButtonHovered] = ImVec4(0.33f, 0.15f, 0.02f, 1.00f);
-  colors[ImGuiCol_ButtonActive] = ImVec4(0.25f, 0.12f, 0.01f, 0.78f);
-  colors[ImGuiCol_Header] = ImVec4(0.25f, 0.12f, 0.01f, 0.78f);
-  colors[ImGuiCol_HeaderHovered] = ImVec4(0.33f, 0.15f, 0.02f, 1.00f);
-  colors[ImGuiCol_HeaderActive] = ImVec4(0.25f, 0.12f, 0.01f, 0.78f);
-  colors[ImGuiCol_Separator] = ImVec4(0.28f, 0.18f, 0.06f, 0.37f);
-  colors[ImGuiCol_SeparatorHovered] = ImVec4(0.33f, 0.15f, 0.02f, 0.17f);
-  colors[ImGuiCol_SeparatorActive] = ImVec4(0.42f, 0.18f, 0.06f, 0.17f);
-  colors[ImGuiCol_ResizeGrip] = ImVec4(0.25f, 0.12f, 0.01f, 0.78f);
-  colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.33f, 0.15f, 0.02f, 1.00f);
-  colors[ImGuiCol_ResizeGripActive] = ImVec4(0.25f, 0.12f, 0.01f, 0.78f);
-  colors[ImGuiCol_Tab] = ImVec4(0.25f, 0.12f, 0.01f, 0.78f);
-  colors[ImGuiCol_TabHovered] = ImVec4(0.33f, 0.15f, 0.02f, 1.00f);
-  colors[ImGuiCol_TabActive] = ImVec4(0.34f, 0.14f, 0.01f, 1.00f);
-  colors[ImGuiCol_TabUnfocused] = ImVec4(0.33f, 0.15f, 0.02f, 1.00f);
-  colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.42f, 0.18f, 0.06f, 1.00f);
+
+  colors[ImGuiCol_Text] = ImVec4(0.64f, 0.37f, 0.37f, 1.00f);
+  colors[ImGuiCol_TextDisabled] = ImVec4(0.49f, 0.26f, 0.26f, 1.00f);
+  colors[ImGuiCol_WindowBg] = ImVec4(0.17f, 0.00f, 0.00f, 0.98f);
+  colors[ImGuiCol_ChildBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+  colors[ImGuiCol_PopupBg] = ImVec4(0.18f, 0.00f, 0.00f, 0.94f);
+  colors[ImGuiCol_Border] = ImVec4(0.35f, 0.00f, 0.03f, 0.50f);
+  colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+  colors[ImGuiCol_FrameBg] = ImVec4(0.14f, 0.04f, 0.00f, 1.00f);
+  colors[ImGuiCol_FrameBgHovered] = ImVec4(0.14f, 0.04f, 0.00f, 1.00f);
+  colors[ImGuiCol_FrameBgActive] = ImVec4(0.14f, 0.04f, 0.00f, 1.00f);
+  colors[ImGuiCol_TitleBg] = ImVec4(0.14f, 0.04f, 0.00f, 1.00f);
+  colors[ImGuiCol_TitleBgActive] = ImVec4(0.14f, 0.04f, 0.00f, 1.00f);
+  colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.00f, 0.00f, 0.00f, 0.51f);
+  colors[ImGuiCol_MenuBarBg] = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+  colors[ImGuiCol_ScrollbarBg] = ImVec4(0.02f, 0.02f, 0.02f, 0.53f);
+  colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.31f, 0.31f, 0.31f, 1.00f);
+  colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.41f, 0.41f, 0.41f, 1.00f);
+  colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.51f, 0.51f, 0.51f, 1.00f);
+  colors[ImGuiCol_CheckMark] = ImVec4(0.87f, 0.23f, 0.09f, 1.00f);
+  colors[ImGuiCol_SliderGrab] = ImVec4(0.87f, 0.23f, 0.09f, 1.00f);
+  colors[ImGuiCol_SliderGrabActive] = ImVec4(1.00f, 0.33f, 0.00f, 1.00f);
+  colors[ImGuiCol_Button] = ImVec4(0.81f, 0.38f, 0.09f, 0.08f);
+  colors[ImGuiCol_ButtonHovered] = ImVec4(0.87f, 0.23f, 0.09f, 1.00f);
+  colors[ImGuiCol_ButtonActive] = ImVec4(1.00f, 0.33f, 0.00f, 1.00f);
+  colors[ImGuiCol_Header] = ImVec4(0.81f, 0.38f, 0.09f, 0.08f);
+  colors[ImGuiCol_HeaderHovered] = ImVec4(0.87f, 0.23f, 0.09f, 1.00f);
+  colors[ImGuiCol_HeaderActive] = ImVec4(1.00f, 0.33f, 0.00f, 1.00f);
+  colors[ImGuiCol_Separator] = ImVec4(0.81f, 0.38f, 0.09f, 0.08f);
+  colors[ImGuiCol_SeparatorHovered] = ImVec4(0.87f, 0.23f, 0.09f, 1.00f);
+  colors[ImGuiCol_SeparatorActive] = ImVec4(1.00f, 0.33f, 0.00f, 1.00f);
+  colors[ImGuiCol_ResizeGrip] = ImVec4(0.81f, 0.38f, 0.09f, 0.08f);
+  colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.87f, 0.23f, 0.09f, 1.00f);
+  colors[ImGuiCol_ResizeGripActive] = ImVec4(1.00f, 0.33f, 0.00f, 1.00f);
+  colors[ImGuiCol_Tab] = ImVec4(0.81f, 0.38f, 0.09f, 0.08f);
+  colors[ImGuiCol_TabHovered] = ImVec4(0.87f, 0.23f, 0.09f, 1.00f);
+  colors[ImGuiCol_TabActive] = ImVec4(1.00f, 0.33f, 0.00f, 1.00f);
+  colors[ImGuiCol_TabUnfocused] = ImVec4(0.81f, 0.38f, 0.09f, 0.08f);
+  colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.81f, 0.38f, 0.09f, 0.08f);
   colors[ImGuiCol_PlotLines] = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
   colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
   colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
   colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
-  colors[ImGuiCol_TextSelectedBg] = ImVec4(0.06f, 0.03f, 0.01f, 0.78f);
-  colors[ImGuiCol_DragDropTarget] = ImVec4(0.64f, 0.42f, 0.09f, 0.90f);
-  colors[ImGuiCol_NavHighlight] = ImVec4(0.64f, 0.42f, 0.09f, 0.90f);
+  colors[ImGuiCol_TextSelectedBg] = ImVec4(0.81f, 0.38f, 0.09f, 0.08f);
+  colors[ImGuiCol_DragDropTarget] = ImVec4(1.00f, 1.00f, 0.00f, 0.90f);
+  colors[ImGuiCol_NavHighlight] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
   colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.00f, 1.00f, 1.00f, 0.70f);
   colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.20f);
   colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.35f);
-
-  ImGuiStyle &style = ImGui::GetStyle();
-
-  style.TabRounding = 2;
-  style.FrameRounding = 2;
-  style.WindowPadding.x = 0;
-  style.WindowPadding.y = 0;
-  style.FramePadding.x = 1;
-  style.FramePadding.y = 0;
-  style.IndentSpacing = 8;
-  style.WindowRounding = 3;
-  style.ScrollbarSize = 10;
 }
 
 void engine::quit_conf(bool *open) {
@@ -282,10 +268,10 @@ void engine::quit_conf(bool *open) {
     // create centered window
     ImGui::SetNextWindowPos(
         ImVec2(total_screen_width / 2 - 120, total_screen_height / 2 - 25));
-    ImGui::SetNextWindowSize(ImVec2(230, 35));
+    ImGui::SetNextWindowSize(ImVec2(300, 55));
     ImGui::Begin("quit", open, flags);
 
-    ImGui::Text("  Are you sure you want to quit?  ");
+    ImGui::Text("Are you sure you want to quit?");
 
     ImGui::Text("  ");
     ImGui::SameLine();
@@ -439,6 +425,159 @@ static void HelpMarker(const char *desc) {
   }
 }
 
+// Demonstrate using DockSpace() to create an explicit docking node within an
+// existing window. Note that you dock windows into each others _without_ a
+// dockspace, by just clicking on a window title bar and moving it (+ hold SHIFT
+// if io.ConfigDockingWithShift is set). DockSpace() and DockSpaceOverViewport()
+// are only useful to construct a central docking location for your application.
+void ShowExampleAppDockSpace(bool *p_open) {
+  // In 99% case you should be able to just call DockSpaceOverViewport() and
+  // ignore all the code below! In this specific demo, we are not using
+  // DockSpaceOverViewport() because:
+  // - we allow the host window to be floating/moveable instead of filling the
+  // viewport (when opt_fullscreen == false)
+  // - we allow the host window to have padding (when opt_padding == true)
+  // - we have a local menu bar in the host window (vs. you could use
+  // BeginMainMenuBar() + DockSpaceOverViewport() in your code!) TL;DR; this
+  // demo is more complicated than what you would normally use. If we removed
+  // all the options we are showcasing, this demo would become:
+  //     void ShowExampleAppDockSpace()
+  //     {
+  //         ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+  //     }
+
+  static bool opt_fullscreen = true;
+  static bool opt_padding = false;
+  static ImGuiDockNodeFlags dockspace_flags =
+      ImGuiDockNodeFlags_None ^ ImGuiDockNodeFlags_PassthruCentralNode;
+
+  // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window
+  // not dockable into, because it would be confusing to have two docking
+  // targets within each others.
+  ImGuiWindowFlags window_flags =
+      ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+  if (opt_fullscreen) {
+    ImGuiViewport *viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->GetWorkPos());
+    ImGui::SetNextWindowSize(viewport->GetWorkSize());
+    ImGui::SetNextWindowViewport(viewport->ID);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+    window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
+                    ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+    window_flags |=
+        ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+  } else {
+    dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+  }
+
+  // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render
+  // our background and handle the pass-thru hole, so we ask Begin() to not
+  // render a background.
+  if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+    window_flags |= ImGuiWindowFlags_NoBackground;
+
+  // Important: note that we proceed even if Begin() returns false (aka window
+  // is collapsed). This is because we want to keep our DockSpace() active. If a
+  // DockSpace() is inactive, all active windows docked into it will lose their
+  // parent and become undocked. We cannot preserve the docking relationship
+  // between an active window and an inactive docking, otherwise any change of
+  // dockspace/settings would lead to windows being stuck in limbo and never
+  // being visible.
+  if (!opt_padding)
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+  ImGui::Begin("DockSpace Demo", p_open, window_flags);
+  if (!opt_padding)
+    ImGui::PopStyleVar();
+
+  if (opt_fullscreen)
+    ImGui::PopStyleVar(2);
+
+  // DockSpace
+  ImGuiIO &io = ImGui::GetIO();
+  if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
+    ImGuiID dockspace_id = ImGui::GetID("DockSpace");
+    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+  }
+
+  if (ImGui::BeginMenuBar()) {
+    if (ImGui::BeginMenu("Options")) {
+      // Disabling fullscreen would allow the window to be moved to the front of
+      // other windows, which we can't undo at the moment without finer window
+      // depth/z control.
+      ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen);
+      ImGui::MenuItem("Padding", NULL, &opt_padding);
+      ImGui::Separator();
+
+      if (ImGui::MenuItem("Flag: NoSplit", "",
+                          (dockspace_flags & ImGuiDockNodeFlags_NoSplit) !=
+                              0)) {
+        dockspace_flags ^= ImGuiDockNodeFlags_NoSplit;
+      }
+      if (ImGui::MenuItem("Flag: NoResize", "",
+                          (dockspace_flags & ImGuiDockNodeFlags_NoResize) !=
+                              0)) {
+        dockspace_flags ^= ImGuiDockNodeFlags_NoResize;
+      }
+      if (ImGui::MenuItem("Flag: NoDockingInCentralNode", "",
+                          (dockspace_flags &
+                           ImGuiDockNodeFlags_NoDockingInCentralNode) != 0)) {
+        dockspace_flags ^= ImGuiDockNodeFlags_NoDockingInCentralNode;
+      }
+      if (ImGui::MenuItem(
+              "Flag: AutoHideTabBar", "",
+              (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0)) {
+        dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar;
+      }
+      if (ImGui::MenuItem(
+              "Flag: PassthruCentralNode", "",
+              (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0,
+              opt_fullscreen)) {
+        dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode;
+      }
+      ImGui::Separator();
+
+      if (ImGui::MenuItem("Close", NULL, false, p_open != NULL))
+        *p_open = false;
+      ImGui::EndMenu();
+    }
+    HelpMarker("When docking is enabled, you can ALWAYS dock MOST window into "
+               "another! Try it now!"
+               "\n\n"
+               " > if io.ConfigDockingWithShift==false (default):"
+               "\n"
+               "   drag windows from title bar to dock"
+               "\n"
+               " > if io.ConfigDockingWithShift==true:"
+               "\n"
+               "   drag windows from anywhere and hold Shift to dock"
+               "\n\n"
+               "This demo app has nothing to do with it!"
+               "\n\n"
+               "This demo app only demonstrate the use of ImGui::DockSpace() "
+               "which allows you to manually create a docking node _within_ "
+               "another window. This is useful so you can decorate your main "
+               "application window (e.g. with a menu bar)."
+               "\n\n"
+               "ImGui::DockSpace() comes with one hard constraint: it needs to "
+               "be submitted _before_ any window which may be docked into it. "
+               "Therefore, if you use a dock spot as the central point of your "
+               "application, you'll probably want it to be part of the very "
+               "first window you are submitting to imgui every frame."
+               "\n\n"
+               "(NB: because of this constraint, the implicit \"Debug\" window "
+               "can not be docked into an explicit DockSpace() node, because "
+               "that window is submitted as part of the NewFrame() call. An "
+               "easy workaround is that you can create your own implicit "
+               "\"Debug##2\" window after calling DockSpace() and leave it in "
+               "the window stack for anyone to use.)");
+
+    ImGui::EndMenuBar();
+  }
+
+  ImGui::End();
+}
+
 void engine::draw_everything() {
   ImGuiIO &io = ImGui::GetIO();
   (void)io; // void cast prevents unused variable warning
@@ -464,50 +603,58 @@ void engine::draw_everything() {
   ImGui_ImplSDL2_NewFrame(window);
   ImGui::NewFrame();
 
+  static bool show_dockspace = true;
+  if (show_dockspace)
+    ShowExampleAppDockSpace(&show_dockspace);
 
   // show quit confirm window
   quit_conf(&quitconfirm);
 
   // show the demo window
-  // static bool show_demo_window = true;
-  // if (show_demo_window)
-  //   ImGui::ShowDemoWindow(&show_demo_window);
+  static bool show_demo_window = true;
+  if (show_demo_window)
+    ImGui::ShowDemoWindow(&show_demo_window);
 
   // do my own windows
-  ImGui::Begin("Controls", NULL, 0);
+  ImGui::Begin("Controls0", NULL, 0);
   ImGui::Text("This is some text");
   ImGui::End();
 
-  // ImGui::Begin("Editor", NULL, 0);
-  // static TextEditor editor;
-  // // static auto lang = TextEditor::LanguageDefinition::CPlusPlus();
-  // static auto lang = TextEditor::LanguageDefinition::GLSL();
-  // editor.SetLanguageDefinition(lang);
+  ImGui::Begin("Controls1", NULL, 0);
+  ImGui::Text("This is some text");
+  ImGui::End();
 
-  // auto cpos = editor.GetCursorPosition();
-  // // editor.SetPalette(TextEditor::GetLightPalette());
-  // editor.SetPalette(TextEditor::GetDarkPalette());
-  // // editor.SetPalette(TextEditor::GetRetroBluePalette());
+  ImGui::Begin("Editor", NULL, 0);
 
-  // static const char *fileToEdit = "resources/engine_code/shaders/blit.vs.glsl";
-  // std::ifstream t(fileToEdit);
-  // static bool loaded = false;
-  // if (!loaded) {
-  //   editor.SetLanguageDefinition(lang);
-  //   if (t.good()) {
-  //     editor.SetText(std::string((std::istreambuf_iterator<char>(t)),
-  //                                std::istreambuf_iterator<char>()));
-  //     loaded = true;
-  //   }
-  // }
+  static TextEditor editor;
+  // static auto lang = TextEditor::LanguageDefinition::CPlusPlus();
+  static auto lang = TextEditor::LanguageDefinition::GLSL();
+  editor.SetLanguageDefinition(lang);
 
-  // ImGui::Text("%6d/%-6d %6d lines  | %s | %s | %s | %s", cpos.mLine + 1,
-  //             cpos.mColumn + 1, editor.GetTotalLines(),
-  //             editor.IsOverwrite() ? "Ovr" : "Ins",
-  //             editor.CanUndo() ? "*" : " ",
-  //             editor.GetLanguageDefinition().mName.c_str(), fileToEdit);
+  auto cpos = editor.GetCursorPosition();
+  // editor.SetPalette(TextEditor::GetLightPalette());
+  editor.SetPalette(TextEditor::GetDarkPalette());
+  // editor.SetPalette(TextEditor::GetRetroBluePalette());
 
-  // editor.Render("TextEditor");
+  static const char *fileToEdit = "resources/engine_code/shaders/blit.vs.glsl";
+  std::ifstream t(fileToEdit);
+  static bool loaded = false;
+  if (!loaded) {
+    editor.SetLanguageDefinition(lang);
+    if (t.good()) {
+      editor.SetText(std::string((std::istreambuf_iterator<char>(t)),
+                                 std::istreambuf_iterator<char>()));
+      loaded = true;
+    }
+  }
+
+  ImGui::Text("%6d/%-6d %6d lines  | %s | %s | %s | %s", cpos.mLine + 1,
+              cpos.mColumn + 1, editor.GetTotalLines(),
+              editor.IsOverwrite() ? "Ovr" : "Ins",
+              editor.CanUndo() ? "*" : " ",
+              editor.GetLanguageDefinition().mName.c_str(), fileToEdit);
+
+  editor.Render("TextEditor");
   ImGui::End();
 
   // get it ready to put on the screen

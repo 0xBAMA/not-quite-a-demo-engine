2141b2ca6032ef859f7f5b2c166cafae231f9f96
jb <jonbaker1222@gmail.com>
2021-12-28 13:17:04 -0600

    formatting and removal of .use() methods in shader.h

--- resources/engineCode/shaders/shader.h
+++ resources/engineCode/shaders/shader.h
@@ -5,7 +5,7 @@
 #ifndef SHADER_H
 #define SHADER_H
 
-#include "includes.h"
+#include "../includes.h"
 
 #include <vector>
 #include <cmath>
@@ -21,17 +21,14 @@
 using std::cin;
 using std::cout;
 using std::cerr;
-
 using std::flush;
 using std::endl;
 
-class Shader
-{
+class Shader {
   public:
     GLuint Program;
     // Constructor generates the shader on the fly
-    Shader( const GLchar *vertexPath, const GLchar *fragmentPath, bool verbose=false)
-    {
+    Shader( const GLchar *vertexPath, const GLchar *fragmentPath, bool verbose = false ) {
 
         // 1. Retrieve the vertex/fragment source code from filePath
         std::string vertexCode;
@@ -41,8 +38,7 @@ class Shader
         // ensures ifstream objects can throw exceptions:
         vShaderFile.exceptions ( std::ifstream::badbit );
         fShaderFile.exceptions ( std::ifstream::badbit );
-        try
-        {
+        try {
             // Open files
             vShaderFile.open( vertexPath );
             fShaderFile.open( fragmentPath );
@@ -56,20 +52,16 @@ class Shader
             // Convert stream into string
             vertexCode = vShaderStream.str( );
             fragmentCode = fShaderStream.str( );
+        } catch ( std::ifstream::failure &e ) {
+            cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << endl;
         }
-        catch ( std::ifstream::failure &e )
-        {
-            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
-        }
-
 
         const GLchar *vShaderCode = vertexCode.c_str( );
         const GLchar *fShaderCode = fragmentCode.c_str( );
         // 2. Compile shaders
         GLuint vertex, fragment;
         GLint success;
-        GLchar infoLog[512];
-
+        GLchar infoLog[ 512 ];
 
         // Vertex Shader
         vertex = glCreateShader( GL_VERTEX_SHADER );
@@ -78,13 +70,10 @@ class Shader
 
         // Print compile errors if any
         glGetShaderiv( vertex, GL_COMPILE_STATUS, &success );
-        if ( !success )
-        {
+        if ( !success ) {
             glGetShaderInfoLog( vertex, 512, NULL, infoLog );
-            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+            cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
         }
-
-
 
         // Fragment Shader
         fragment = glCreateShader( GL_FRAGMENT_SHADER );
@@ -93,12 +82,10 @@ class Shader
 
         // Print compile errors if any
         glGetShaderiv( fragment, GL_COMPILE_STATUS, &success );
-        if ( !success )
-        {
+        if ( !success ) {
             glGetShaderInfoLog( fragment, 512, NULL, infoLog );
-            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+            cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << endl;
         }
-
 
         // Shader Program
         this->Program = glCreateProgram( );
@@ -106,44 +93,32 @@ class Shader
         glAttachShader( this->Program, fragment );
         glLinkProgram( this->Program );
 
-
         // Print linking errors if any
         glGetProgramiv( this->Program, GL_LINK_STATUS, &success );
-        if (!success)
-        {
+        if (!success) {
             glGetProgramInfoLog( this->Program, 512, NULL, infoLog );
-            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+            cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << endl;
         }
-
 
         // Delete the shaders as they're linked into our program now and no longer necessery
         glDeleteShader( vertex );
         glDeleteShader( fragment );
-
-    }
-    // Uses the current shader
-    void Use( )
-    {
-        glUseProgram( this->Program );
     }
 };
 
 
-class CShader //very similar to above, but for compute shader instead of vertex/fragment
-{
+class CShader { // very similar to above, but for compute shader instead of vertex/fragment
   public:
     GLuint Program;
     // Constructor generates the shader on the fly
-    CShader( const GLchar *Path, bool verbose=false)
-    {
+    CShader( const GLchar *Path, bool verbose = false ) {
 
         // 1. Retrieve the compute shader source code from Path
         std::string Code;
         std::ifstream File;
         // ensures ifstream objects can throw exceptions:
         File.exceptions ( std::ifstream::badbit );
-        try
-        {
+        try {
             // Open files
             File.open( Path );
             std::stringstream ShaderStream;
@@ -153,29 +128,25 @@ class CShader //very similar to above, but for compute shader instead of vertex/
             File.close( );
             // Convert stream into string
             Code = ShaderStream.str( );
-        }
-        catch ( std::ifstream::failure &e )
-        {
-            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+        } catch ( std::ifstream::failure &e ) {
+            cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << endl;
         }
 
         const GLchar *cstrCode = Code.c_str( );
-        // 2. Compile shaders
+        // 2. Compile shader
         GLuint shader;
         GLint success;
-        GLchar infoLog[512];
+        GLchar infoLog[ 512 ];
 
-        // Vertex Shader
         shader = glCreateShader( GL_COMPUTE_SHADER );
         glShaderSource( shader, 1, &cstrCode, NULL );
         glCompileShader( shader );
 
         // Print compile errors if any
         glGetShaderiv( shader, GL_COMPILE_STATUS, &success );
-        if ( !success )
-        {
+        if ( !success ) {
             glGetShaderInfoLog( shader, 512, NULL, infoLog );
-            std::cout << "ERROR::SHADER::COMPUTE::COMPILATION_FAILED\n" << infoLog << std::endl;
+            cout << "ERROR::SHADER::COMPUTE::COMPILATION_FAILED\n" << infoLog << endl;
         }
 
         // Shader Program
@@ -185,19 +156,12 @@ class CShader //very similar to above, but for compute shader instead of vertex/
 
         // Print linking errors if any
         glGetProgramiv( this->Program, GL_LINK_STATUS, &success );
-        if (!success)
-        {
+        if ( !success ) {
             glGetProgramInfoLog( this->Program, 512, NULL, infoLog );
-            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+            cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << endl;
         }
         // Delete the shaders as they're linked into our program now and no longer necessery
         glDeleteShader( shader );
-
-    }
-    // Uses the current shader
-    void Use( )
-    {
-        glUseProgram( this->Program );
     }
 };
 

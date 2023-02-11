f821950e45d2c027f02425a20380a4b9f004d26d
jb <jon.baker1222@gmail.com>
2023-02-11 07:41:05 -0600

    move data structs to header

--- src/engine/includes.h
+++ src/engine/includes.h
@@ -132,31 +132,7 @@ using json = nlohmann::json;
 #include "../Serialization/tinyXML2/tinyxml2.h"
 using XMLDocument = tinyxml2::XMLDocument;
 
-struct configData {
-	uint32_t windowFlags = 0;
-	string windowTitle = string( "NQADE" );
-	int32_t width = 0;
-	int32_t height = 0;
-	bool linearFilter = false;
-	ivec2 windowOffset = ivec2( 0, 0 );
-	uint8_t startOnScreen = 0;
-
-	uint8_t MSAACount = 0;
-	vec4 clearColor = vec4( 0.0f );
-	bool vSyncEnable = true;
-	int numMsDelayAfterCallback = 10;
-	uint8_t OpenGLVersionMajor = 4;
-	uint8_t OpenGLVersionMinor = 3;
-	bool reportPlatformInfo = true;
-	bool enableDepthTesting = false;
-
-	// anything else ... ?
-};
-
-struct colorGradeParameters {
-	int tonemapMode = 6; // todo: write an enum for this
-	float gamma = 1.1f;
-	float colorTemp = 6500.0f;
-};
+// config, etc structs
+#include "./dataStructs.h"
 
 #endif

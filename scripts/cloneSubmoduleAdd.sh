93a21e2e19a82cd789786915f4208f8312b5e432
jb <jon.baker1222@gmail.com>
2023-02-12 22:09:41 -0600

    fixup clonesubmoduleadd script

--- scripts/cloneSubmoduleAdd.sh
+++ scripts/cloneSubmoduleAdd.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 git submodule add https://github.com/Auburn/FastNoise2.git src/noise/FastNoise2
-git submodule add https://github.com/0xBAMA/hoard-of-bitfonts.git src/fonts/hoardOfBitfonts
 git submodule add https://github.com/wolfpld/tracy.git src/tracy
-git submodule add https://github.com/assimp/assimp.git src/ModelLoading/assimp
